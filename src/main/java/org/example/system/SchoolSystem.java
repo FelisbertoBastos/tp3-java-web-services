b6ced89db236ed8a69903f3c434fd5b246d94b52
Felisberto Bastos <felisbertobastos22@gmail.com>
2023-11-26 18:30:58 -0300

    add logback setup

--- src/main/java/org/example/system/SchoolSystem.java
+++ src/main/java/org/example/system/SchoolSystem.java
@@ -2,13 +2,19 @@ package org.example.system;
 
 import org.example.people.Teacher;
 import org.example.subject.Course;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SchoolSystem {
+    private final Logger LOGGER = LoggerFactory.getLogger(SchoolSystem.class);
+
     public void run() {
         Scanner scanner = new Scanner(System.in);
+
+        LOGGER.info("Iniciando o cadastro");
 
         System.out.println("--- Cadastro de Curso ---");
 
@@ -19,8 +25,10 @@ public class SchoolSystem {
                 System.out.print("Nome do curso: ");
                 String courseName = scanner.nextLine();
                 course.setName(courseName);
+                LOGGER.debug("Usuário digitou o nome do curso: " + courseName);
                 break;
             } catch (IllegalArgumentException exception) {
+                LOGGER.error("Usuário digitou nome do curso inválido");
                 System.out.println("Nome inválido");
             }
         }
@@ -31,8 +39,10 @@ public class SchoolSystem {
                 String teacherName = scanner.nextLine();
                 Teacher teacher = new Teacher(teacherName);
                 course.setTeacher(teacher);
+                LOGGER.debug("Usuário digitou o nome do professor: " + teacherName);
                 break;
             } catch (IllegalArgumentException exception) {
+                LOGGER.error("Usuário digitou nome do professor inválido");
                 System.out.println("Nome do professor inválido");
             }
         }
@@ -42,8 +52,10 @@ public class SchoolSystem {
             try {
                 System.out.print("N° de alunos: ");
                 numberOfStudents = scanner.nextInt();
+                LOGGER.debug("Usuário digitou o número de alunos: " + numberOfStudents);
                 break;
             } catch (InputMismatchException exception) {
+                LOGGER.error("Usuário digitou número de alunos inválido");
                 System.out.println("Digite o número de alunos");
             } finally {
                 scanner.nextLine();
@@ -54,9 +66,12 @@ public class SchoolSystem {
             while (true) {
                 try {
                     System.out.printf("%d° aluno: ", i + 1);
-                    course.addStudent(scanner.nextLine());
+                    String studentName = scanner.nextLine();
+                    course.addStudent(studentName);
+                    LOGGER.debug("Usuário digitou o nome do aluno: " + studentName);
                     break;
                 } catch (IllegalArgumentException exception) {
+                    LOGGER.error("Usuário digitou nome de aluno inválido");
                     System.out.println("Nome do aluno inválido");
                 }
             }
@@ -65,5 +80,7 @@ public class SchoolSystem {
         System.out.println("\n--- Dados do Curso ---");
 
         System.out.println(course.getDetails());
+
+        LOGGER.info("Finalizando o cadastro");
     }
 }
